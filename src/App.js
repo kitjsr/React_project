150fced9dd0be6db37bf3ec9eb9835590631585e
ESHA KUMARI <eshakumarijsr2002@gmail.com>
2025-06-21 22:45:26 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,30 +1,58 @@
 
-import { Link, Route, Routes } from 'react-router';
+
+import "./App.css";
+import Home from "./Home";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Register from "./Register";
+import Login from "./Login";
+import Invoice from "./Invoice";
+import Additem from "./Additem";
+import Stock from "./Stock";
+// import Sidebar from './Sidebar';
+import Sidebar from "./Sidebar";
+import Item from "./Item";
+import { Container, Row, Col } from "react-bootstrap";
+
+import { Route, Routes } from 'react-router';
 import './App.css';
-import Home from './Home';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Register from './Register';
-import Login from './Login';
-import Invoice from './Invoice';
-import Additem from './Additem';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Model } from './Model';
+import Party from "./Party";
+import Profile from "./Profile";
 
 function App() {
   return (
     <div>
-      <ul>
+      {/* <div className='App'> */}
+      {/* <Sidebar/> */}
+      {/* </div> */}
+      {/* <ul>
         <li><Link to="/home">Home</Link></li>
         <li><Link to="/register">Register</Link></li>
         <li><Link to="/invoice">Invoice</Link></li>
         <li><Link to="/additem">Item</Link></li>
-      </ul>
-      <Routes>
-        <Route path=" " element={<Home/>}/>
-        <Route path="/register" element={<Register/>}/>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/invoice" element={<Invoice/>}/>
-        <Route path="/additem" element={<Additem/>}/>
-        <Route path="/*" element={<Home/>}/>
-      </Routes>
+      </ul> */}
+
+      <Container>
+        <Row>
+          <Col md={3}>
+            <Sidebar />
+          </Col>
+          <Col>
+            <Routes>
+              <Route path=" " element={<Home />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/invoice" element={<Invoice />} />
+              <Route path="/additem" element={<Additem />} />
+              <Route path="/stock" element={<Stock />} />
+              <Route path="/party" element={<Party />} />
+              <Route path="/profile" element={<Profile/>} />
+              <Route path="/*" element={<Home />} />
+            </Routes>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
